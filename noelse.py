901ad5fe08e1f2204547b30e7bc1cbe6f365956c
makudesu <mark.agustin@achievewithoutborders.com>
2016-08-12 10:14:40 +0800

    Corrected with pep8

--- noelse.py
+++ noelse.py
@@ -1,16 +1,17 @@
-## this proves that you don't have to use else in a function to improve code readability - just exit the code
+## this proves that you don't have to use else in a function to
+## improve code readability - just exit the code
 ## but it's perfectly fine to use else in loops
 ## mark kevin
 
+
 def with_else(x):
-	if x:
-		return True
-	else:
-		return False
+    if x:
+        return True
+    else:
+        return False
+
 
 def without_else(x):
-	if x:
-		print 'x'
-		return True
-	return False
-
+    if x:
+        return True
+    return False
--- test.py
+++ test.py
@@ -1,19 +1,23 @@
 import unittest
 from noelse import with_else, without_else
 
+
 class WithElseTestCase(unittest.TestCase):
 
-	def test_with_else_is_true(self):
-		self.assertTrue(with_else(True))
-	def test_with_else_is_false(self):
-		self.assertFalse(with_else(False))
+    def test_with_else_is_true(self):
+        self.assertTrue(with_else(True))
+
+    def test_with_else_is_false(self):
+        self.assertFalse(with_else(False))
+
 
 class WithoutElseTestCase(unittest.TestCase):
 
-	def test_without_else_is_true(self):
-		self.assertTrue(without_else(True))
-	def test_without_else_is_false(self):
-		self.assertFalse(without_else(False))
+    def test_without_else_is_true(self):
+        self.assertTrue(without_else(True))
+
+    def test_without_else_is_false(self):
+        self.assertFalse(without_else(False))
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
