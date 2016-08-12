fec6e3c717d48e66f72d599017687b0cc9032b2a
makudesu <mark.agustin@achievewithoutborders.com>
2016-08-12 10:04:36 +0800

    Initial commit

---
+++ noelse.py
@@ -0,0 +1,16 @@
+## this proves that you don't have to use else in a function to improve code readability - just exit the code
+## but it's perfectly fine to use else in loops
+## mark kevin
+
+def with_else(x):
+	if x:
+		return True
+	else:
+		return False
+
+def without_else(x):
+	if x:
+		print 'x'
+		return True
+	return False
+
